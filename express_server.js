f9e3e33e1c192106eb4f8ac0b1bee0d2b8621216
Angelo <angvil02@gmail.com>
2024-10-28 19:37:07 -0700

    completed setup of Tinyapp project

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+// set view engine
+app.set('view engine', 'ejs')
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -13,6 +15,10 @@ app.get('/', (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
